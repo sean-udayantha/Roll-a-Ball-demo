0214e728856f7d5193840a43823652b04b2bc122
sean-udayantha <seanudayantha@mail.com>
2022-02-03 00:19:25 +0530

    add the camera movment with object

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,22 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// using UnityEngine.InputSystem;
 public class player : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed;
+    public float speed=0;
     private Rigidbody rb;
+    // private float movementX;
+    // private float movementY;
     void Start()
     {
         rb=GetComponent<Rigidbody> ();
     }
+//   private void OnMove(InputValue movementValue)
+//     {
+//         Vector2 movementVector = movementValue.Get<Vector2>();
 
+//         movementX = movementVector.x;
+//         movementY = movementVector.y;
+//     }
     // Update is called once per frame
-    void Update()
+      void Update()
     {
+         if (Input.GetKeyDown(KeyCode.Space))
+        {
+             GetComponent<Rigidbody>().AddForce(Vector3.up *10,ForceMode.VelocityChange);
+             Debug.Log("pass space");
+        }
+
+        // horizontalnput=Input.GetAxis("Horizontal");
+        // GetComponent<Rigidbody>().velocity=new Vector3(horizontalnput,GetComponent<Rigidbody>().velocity.y,0);
+    }
+    
+    void FixedUpdate()
+    {
+
         float moveHorizontal =Input.GetAxis("Horizontal");
         float moveVertical =Input.GetAxis("Vertical");
+        
 
         Vector3 movement = new Vector3(moveHorizontal,0.0f,moveVertical);
         rb.AddForce(movement*speed);
