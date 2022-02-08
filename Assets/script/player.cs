c450c0c0851219a977a55367432dc851eb820b82
sean-udayantha <seanudayantha@mail.com>
2022-02-08 22:58:30 +0530

    add UI for counting pickup boxs

--- Assets/script/player.cs
+++ Assets/script/player.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 // using UnityEngine.InputSystem;
 public class player : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed=0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
     private Rigidbody rb;
+    private int count;
     // private float movementX;
     // private float movementY;
     void Start()
     {
         rb=GetComponent<Rigidbody> ();
+        count = 0;
+        SetCountText ();
+        winTextObject.SetActive(false);
     }
 //   private void OnMove(InputValue movementValue)
 //     {
@@ -47,6 +54,18 @@ public class player : MonoBehaviour
         if (other.gameObject.CompareTag("PickUp")) 
         {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText ();
         }
     }
+     void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString();
+
+		if (count >= 8) 
+		{
+                    // Set the text value of your 'winText'
+                    winTextObject.SetActive(true);
+		}
+	}
 }
